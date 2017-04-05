b3f8bda461d14479b3fa09b0db731bacf9e26766
Luca Antiga <luca.antiga@orobix.com>
2017-04-05 10:55:59 -0400

    Add Div and DivConstant

--- torch2c/emitters.py
+++ torch2c/emitters.py
@@ -376,6 +376,50 @@ class MulConstant(Emitter):
 register(MulConstant, torch.autograd._functions.basic_ops.MulConstant)
 
 
+class Div(Emitter):
+
+    def __init__(self, obj, prevfns):
+        Emitter.__init__(self, obj, prevfns)
+        self.def_vars({'input0': id(prevfns[0]),
+                       'input1': id(prevfns[1])})
+        self.infer_type_var = 'input0'
+
+    def call_tpl(self):
+        return '''
+            TH${T}Tensor *$id = TH${T}Tensor_new();
+            TH${T}Tensor_cdiv($id,$input0,$input1);
+            '''
+
+    def free_tpl(self):
+        return '''
+            TH${T}Tensor_free($id);
+            '''
+
+register(Div, torch.autograd._functions.basic_ops.Div)
+
+
+class DivConstant(Emitter):
+
+    def __init__(self, obj, prevfns):
+        Emitter.__init__(self, obj, prevfns)
+        self.def_vars({'input': id(prevfns[0])})
+        self.infer_type_var = 'input'
+        self.def_args({'constant': obj.constant})
+
+    def call_tpl(self):
+        return '''
+            TH${T}Tensor *$id = TH${T}Tensor_new();
+            TH${T}Tensor_div($id,$input,$constant);
+            '''
+
+    def free_tpl(self):
+        return '''
+            TH${T}Tensor_free($id);
+            '''
+
+register(DivConstant, torch.autograd._functions.basic_ops.DivConstant)
+
+
 class Softmax(Emitter):
 
     def __init__(self, obj, prevfns):
